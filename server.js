f902ea9e8d2ecb054842d45fec238d90037fe8b7
DEdouard22 <dedouard@live.com>
2019-06-25 23:58:05 -0400

    Added routes for users, auth, profile and posts

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-//Connect Database
+// Connect Database
 connectDB();
 
-//initial set up test with local and Postman
+// initial set up test with local and Postman
 app.get("/", (req, res) => res.send("API Running"));
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
